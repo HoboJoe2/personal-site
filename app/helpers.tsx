16cc9d012f32ab93cf6ed1d3783b1b75fe14188e
Joe Dickson <joedickson4556@gmail.com>
2025-11-14 00:40:11 +1000

    working on implementing mtg legends guessing game

---
+++ app/helpers.tsx
@@ -0,0 +1,30 @@
+'use server';
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function downloadScryfallData() {
+    const data = await fetch('https://api.scryfall.com/bulk-data')
+        .then(res => res.json());
+
+    console.log('Fetched Scryfall bulk data metadata:');
+    console.log(data);
+
+    const metadataPath = path.join(process.cwd(), 'app', 'scryfall', 'metadata.json');
+    const metadataRaw = await fs.readFile(metadataPath, 'utf-8');
+    const metadata = JSON.parse(metadataRaw);
+
+    if (data.data[0].updated_at !== metadata.oracleCardsLastUpdated) {
+        console.log('Downloading updated oracle cards data...');
+        const res = await fetch(data.data[0].download_uri)
+            .then(res => res.json());
+
+        const cardsPath = path.join(process.cwd(), 'app', 'scryfall', 'scryfall_oracle_cards.json');
+        await fs.writeFile(cardsPath, JSON.stringify(res));
+    } else {
+        console.log('Oracle cards data is up to date.');
+    }
+
+    metadata.oracleCardsLastUpdated = data.data[0].updated_at;
+    await fs.writeFile(metadataPath, JSON.stringify(metadata, null, 2));
+    console.log('Scryfall data updated successfully.');
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
+'use client';
+import path from 'path';
+import fs from 'fs/promises';
+import { downloadScryfallData } from './helpers';
+
+export function DownloadButton() {
+  const handleClick = async () => {
+    await downloadScryfallData();
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Download Scryfall Data
+    </button>
+  );
+}
+
+export async function LegendGuessingGame() {
+  const cardsPath = path.join(process.cwd(), 'app', 'scryfall', 'scryfall_oracle_cards.json');
+  const cardsRaw = await fs.readFile(cardsPath, 'utf-8');
+  const cards = JSON.parse(cardsRaw);
+  for (let card in cards) {
+    console.log(card);
+  }
+  return (
+    <div>
+      <h2>Legend Guessing Game</h2>
+      <p>Guess the legendary creature based on its description!</p>
+      {/* Game implementation goes here */}
+    </div>
+  );
+}
+
 export default function Home() {
   return (
-    <p>This is the home page!</p>
+    <>
+      <p>This is the home page!</p>
+      <DownloadButton />
+      <LegendGuessingGame />
+    </>
   );
 }
